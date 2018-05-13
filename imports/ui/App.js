467071088f8f33f7d93496eb0a8a7cf1fa170a91
sarahrichards <righodaro@synergyglobalconsulting.co.uk>
2018-05-13 17:11:28 +0100

    add hide checkbox - snapshot of meteor tutorial

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -8,6 +8,13 @@ import Task from './Task.js';
 
 // App component - represent the whole app
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            hideCompleted: false,
+        };
+    }
 
     handleSubmit(event) {
         event.preventDefault();
@@ -24,9 +31,20 @@ class App extends Component {
         ReactDOM.findDOMNode(this.refs.textInput).value = '';
     }
 
+    toggleHideCompleted() {
+        this.setState({
+            hideCompleted: !this.state.hideCompleted,
+        });
+    }
+
     renderTasks() {
-       return this.props.tasks.map((task) => (
-         <Task key={task._id} task={task} />
+
+        let filteredTasks = this.props.tasks;
+        if (this.state.hideCompleted) {
+            filteredTasks = filteredTasks.filter(task => !task.checked);
+        }
+        return filteredTasks.map((task) => (
+            <Task key={task._id} task={task} />
        ));
     }
 
@@ -35,6 +53,17 @@ class App extends Component {
           <div className="container">
             <header>
                 <h1>Performance Form List</h1>
+
+                <label className="hide-completed">
+                    <input
+                        type="checkbox"
+                        readOnly
+                        checked={this.state.hideCompleted}
+                        onClick={this.toggleHideCompleted.bind(this)}
+                    />
+                    Hide Completed Tasks
+                </label>
+
                 <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
                     <input
                         type="text"
